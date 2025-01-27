90c5eb8941857a5f744b60eab1a10557e7b9a433
Boss0113 <yugsgilles@gmail.com>
2025-01-27 09:39:27 +0100

    Romove CMake files and folder

--- RPS_GAME/build/CMakeFiles/rebuild_and_run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rebuild_and_run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rebuild_and_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
